b9e6edb3df3aa43e88f0d3486edc9be0bdd32953
Jadesola Bejide <vm21106@bristol.ac.uk>
2023-02-27 11:33:03 +0000

    Feature - add deployment files

--- api.py
+++ api.py
@@ -28,12 +28,7 @@ class System:
 app.include_router(router)
 
 origins = [
-    "http://localhost/",
-    "http://127.0.0.1:8000",
-    "http://localhost:8000",
-    "http://localhost/",
-    "http://127.0.0.1:8080",
-    "http://localhost:8080",
+    "jadesolabejide.dev/DreamJournal"
 ]
 
 app.add_middleware(
